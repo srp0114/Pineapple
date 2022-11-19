d8b4bfa51a460f42b2f0b81bb4db30eb464c0c30
srp0114 <srp0114@naver.com>
2022-11-19 16:41:00 +0900

    change search library

--- test/src/components/layout/Search.js
+++ test/src/components/layout/Search.js
@@ -1,28 +1,39 @@
-import React from "react";
-import "antd/dist/antd.css";
-import { Input, Space } from "antd";
-import Chat from "./Chat.js";
-import "../style/Search.css";
-import { AiOutlineUser } from "react-icons/ai";
+import React from 'react';
+import 'antd/dist/antd.css';
+import {  Space } from 'antd';
+import Chat from "./Chat.js"
+import "../style/Search.css"
+import {AiOutlineUser} from "react-icons/ai";
 import { useNavigate } from "react-router-dom";
-const { Search } = Input;
+import { Input } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+//const { Search } = Input;
 
 const App = () => {
-  const navigate = useNavigate();
-  const navigateToMyPage = () => {
-    navigate("/mypage");
-  };
-  return (
-    <Space className="search">
-      <div>
-        <Search placeholder="교환하고 싶은 물건은?" style={{ width: 300 }} />
-      </div>
-      <div onClick={navigateToMyPage} className="icon">
-        <AiOutlineUser size="32" />
-      </div>
-      <Chat />
-    </Space>
-  );
+    const navigate = useNavigate();
+    const navigateToMyPage = () => {
+        navigate("/mypage");
 };
+    return (
+        <>
+        <Space className="search">
+            <div>
+            <ChakraProvider>
+
+            {/*  <Search placeholder="교환하고 싶은 물건은?"  style={{ width: 300 }} />*/}
+            <Input focusBorderColor='#285943 ' placeholder='교환하고 싶은 물건은?' />
+            </ChakraProvider>
+
+            </div>
+            <div onClick={navigateToMyPage} className="icon">
+                <AiOutlineUser size="32"/>
+            </div>
+            <Chat/>
+        </Space>
+    </>
+    );
+};
+
 
 export default App;
