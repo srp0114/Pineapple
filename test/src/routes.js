1bc017d536f54288a03707c8118f2a9c2f6ce150
sjhui0523 <92327500+sjhui0523@users.noreply.github.com>
2022-11-24 12:53:42 +0900

    post route 수정2

--- test/src/routes.js
+++ test/src/routes.js
@@ -3,14 +3,14 @@ const Home = React.lazy(() => import("./components/pages/Home"));
 const Shop = React.lazy(() => import("./components/pages/Shop"));
 const About = React.lazy(() => import("./components/pages/About"));
 //const Login = React.lazy(() => import("./components/pages/Login"));
-const Post = React.lazy(() => import("./components/pages/Post"));
+//const Post = React.lazy(() => import("./components/pages/Post"));
 
 const routes = [
   { path: "/", component: Home, name: "Home" },
   { path: "shop", component: Shop, name: "Shop" },
   { path: "about", component: About, name: "About" },
   //{ path: "login", component: Login, name: "Login" },
-  { path: "post", component: Post, name: "Post" }
+  //{ path: "post", component: Post, name: "Post" }
 ];
 
 export default routes;
