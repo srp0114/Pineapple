3534873b062a7d70c76ed0ce21b8dd9999eac169
JIYun1101 <gina7440@naver.com>
2022-11-24 12:23:09 +0900

    registerPage error

--- test/src/Header.js
+++ test/src/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
 
             {/* <Route path="/about/*" element={<RegisterPage />}></Route> */}
             
-            Route path="/login" element={<Login />}></Route>
+            <Route path="/login" element={<Login />}></Route>
             <Route path="/login/*" element={<RegisterPage />}></Route>
             <Route path="/mypage" element={<MyPage />}></Route>
 
