495bc4155f8f255ce6476549b377a4e44856d8d5
skc4234 <skc4234@naver.com>
2022-11-26 20:23:29 +0900

    회원가입 firebase 연동

--- test/src/components/pages/RegisterPage.js
+++ test/src/components/pages/RegisterPage.js
@@ -4,6 +4,10 @@ import "../style/Login.css";
 import { Input } from "@chakra-ui/react";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import { authService } from "../../service/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { setUser } from "../../helper/database";
+
 export default function RegisterPage() {
   const [email, setEmail] = useState("");
   const [pw, setPw] = useState("");
@@ -34,16 +38,16 @@ export default function RegisterPage() {
       setEmailValid(false);
     }
   };
-  const handleName = (e) => {
-    setName(e.target.value);
-    const regex =
-      /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
-    if (regex.test(e.target.value)) {
-      setEmailValid(true);
-    } else {
-      setEmailValid(false);
-    }
-  };
+  // const handleName = (e) => {
+  //   setName(e.target.value);
+  //   const regex =
+  //     /^(([^<>()\[\].,;:\s@"]+(\.[^<>()\[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+  //   if (regex.test(e.target.value)) {
+  //     setEmailValid(true);
+  //   } else {
+  //     setEmailValid(false);
+  //   }
+  // };
   const handlePw = (e) => {
     setPw(e.target.value);
     const regex =
@@ -54,8 +58,17 @@ export default function RegisterPage() {
       setPwValid(false);
     }
   };
-  const onClickConfirmButton = () => {
-    alert("등록에 성공했습니다.");
+  const onClickConfirmButton = async() => {
+    if(email !== "" && pw !== "" && name !== "") {
+      try {
+        const res = await createUserWithEmailAndPassword(authService, email, pw)
+        await setUser({email: email, name: name, uid: res.user.uid})
+        alert("등록에 성공했습니다.");
+      } catch (eroor) {
+        alert("등록에 실패했습니다.");
+      }
+    }
+
   };
 
   return (
@@ -96,9 +109,9 @@ export default function RegisterPage() {
                 fontSize="1em"
                 className="input"
                 type="text"
-                placeholder="test@gmail.com"
+                placeholder="2~10자의 한글, 영문, 숫자"
                 value={name}
-                onChange={handleName}
+                onChange={event => setName(event.target.value)}
               ></Input>
             </ChakraProvider>
           </div>
