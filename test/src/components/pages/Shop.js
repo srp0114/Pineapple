da4fef7f262b22ba18c53f510ea99d68c1107e13
JIYun1101 <gina7440@naver.com>
2022-11-27 01:40:12 +0900

    bomb

--- test/src/components/pages/Shop.js
+++ test/src/components/pages/Shop.js
@@ -1,26 +1,38 @@
-import React, { useState }from "react";
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup } from '@chakra-ui/react'
-import {Button} from "antd"
+import React, { useState } from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Image,
+  Stack,
+  Heading,
+  Text,
+  Divider,
+  ButtonGroup,
+} from "@chakra-ui/react";
+import { Button } from "antd";
 import { Global } from "@emotion/react";
-import data from '../data.js';
+import data from "../data.js";
 import "../style/Shop.css";
 import { Link } from "react-router-dom";
-import Nav from "../layout/Nav"
-import PostUploadBtn from "../layout/PostUploadBtn"
-import Fab from '@mui/material/Fab';
-import AddIcon from '@mui/icons-material/Add';
+import Nav from "../layout/Nav";
+import PostUploadBtn from "../layout/PostUploadBtn";
+import Fab from "@mui/material/Fab";
+import AddIcon from "@mui/icons-material/Add";
 import styled from "styled-components";
 
 const Fonts = () => (
-        <Global styles = {`
+  <Global
+    styles={`
             @font-face {
             font-family: "NanumSquare";
             src: url("../fonts/NanumSquareNeo-cBd.ttf");
         }
-        `} 
-    />
-)
+        `}
+  />
+);
 
 const ButtonContainer = styled.div`
   .ant-btn-primary {
@@ -35,62 +47,69 @@ const ButtonContainer = styled.div`
   }
   .ant-btn-primary:focus {
     background-color: #285943;
-    border-color: #285943; 
+    border-color: #285943;
   }
 `;
 
 const Shop = () => {
-    const [cards, setCards] = useState(data);
-    const theme = extendTheme({fonts:{heading:'NanumSquare'},})
+  const [cards, setCards] = useState(data);
+  const theme = extendTheme({ fonts: { heading: "NanumSquare" } });
 
-    return (
-        <>
-        <Nav/>
+  return (
+    <>
+      <Nav />
 
-        <PostUploadBtn />
+      <PostUploadBtn />
 
-        <div className="cards">
-            {cards.map((card, i) => {
-                return (
-                    <ChakraProvider theme ={theme}>
-                    <Card maxW='sm' className="cardStyle">
-                        <CardBody key={i}>
-                            <Image
-                            src={card.image}
-                            borderRadius='lg'
-                            />
-                            <Stack mt='4' spacing='3'>
-                                <Heading size='md' color='#285943'>{card.title}</Heading>
-                                <Text>{card.change}</Text>
-                                <Text>{card.address}</Text>
-                            </Stack>
-                        </CardBody>
-                        <Divider />
-                        <CardFooter>
-                            <ButtonGroup spacing='2'>
-                            <ButtonContainer>
-
-                            <Link to='/post' state={{data: card}}>
-                                <Button type="primary" shape="round" size="default"
-                                     >
-                                    Details
-                                </Button>
-                            </Link>
-                                <Button type="primary" shape="round" size="default"
-                                    style={{margin:"5px"}}>
-                                    AddToCart
-                                </Button>
-                            </ButtonContainer>
-                            </ButtonGroup>
-                        </CardFooter>
-                    </Card>
-                    </ChakraProvider>
-                )
-            })}
+      <div className="cards">
+        {cards.map((card, i) => {
+          return (
+            <ChakraProvider theme={theme}>
+              <Card maxW="sm" className="cardStyle">
+                <div class="bomb"></div>
+                <CardBody key={i}>
+                    {/* <div class="badge_right">
+                    <Badge>
+                      <div class="timer">
+                        <div class="mask"></div>
+                    </Badge>
+                  </div> */}
+                  <Image src={card.image} borderRadius="lg" />
+                  <Stack mt="4" spacing="3">
+                    <Heading size="md" color="#285943">
+                      {card.title}
+                    </Heading>
+                    <Text>{card.change}</Text>
+                    <Text>{card.address}</Text>
+                  </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter>
+                  <ButtonGroup spacing="2">
+                    <ButtonContainer>
+                      <Link to="/post" state={{ data: card }}>
+                        <Button type="primary" shape="round" size="default">
+                          Details
+                        </Button>
+                      </Link>
+                      <Button
+                        type="primary"
+                        shape="round"
+                        size="default"
+                        style={{ margin: "5px" }}
+                      >
+                        AddToCart
+                      </Button>
+                    </ButtonContainer>
+                  </ButtonGroup>
+                </CardFooter>
+              </Card>
+            </ChakraProvider>
+          );
+        })}
       </div>
-     </>
-    )
-}
+    </>
+  );
+};
 
 export default Shop;
-
---
+++ test/src/components/pages/bomb.js
@@ -0,0 +1,17 @@
+import "../style/Login.css";
+import "../style/Shop.css";
+// import bomb1 from "../bomb_animation/bomb2_1.jpg";
+// import bomb2 from "../bomb_animation/bomb2_2.jpg";
+// import bomb3 from "../bomb_animation/bomb2_3.jpg";
+// import bomb4 from "../bomb_animation/bomb2_4.jpg";
+// import bomb5 from "../bomb_animation/bomb2_5.jpg";
+// import bomb6 from "../bomb_animation/bomb2_6.jpg";
+// import bomb7 from "../bomb_animation/bomb2_7.jpg";
+// import bomb8 from "../bomb_animation/bomb2_8.jpg";
+// import bomb9 from "../bomb_animation/bomb2_9.jpg";
+
+const bomb = () => {
+  return <div class="bomb"></div>;
+};
+
+export default bomb;
